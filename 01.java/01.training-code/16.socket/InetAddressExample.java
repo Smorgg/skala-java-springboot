8c9020ba618c7d7fe096af695d1016c1e2567ed8
himang10 <yongwooyi@gmail.com>
2026-08-06 14:45:28 +0900

    optimization

--- 01.java/01.training-code/16.socket/InetAddressExample.java
+++ 01.java/01.training-code/16.socket/InetAddressExample.java
@@ -3,7 +3,7 @@ import java.net.InetAddress;
 public class InetAddressExample {
     public static void main(String[] args) throws Exception {
         // 도메인 이름으로 IP 조회
-        InetAddress harbor = InetAddress.getByName("amdp-registry.skala-ai.com");
+        InetAddress harbor = InetAddress.getByName("skala-registry.skala-ai.com");
         System.out.println("Host Name : " + harbor.getHostName());
         System.out.println("Host Address : " + harbor.getHostAddress());
 
