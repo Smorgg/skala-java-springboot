96ed590cf10f34c6358578fc8af9554e1b2333b0
Smorgg <ka167954@gmail.com>
2026-08-13 13:41:38 +0900

    feat: profile 적용

---
+++ mycode/myapp/src/main/java/com/sk/skala/myapp/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.sk.skala.myapp.controller;
+
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sk.skala.myapp.domain.User;
+import com.sk.skala.myapp.service.UserService;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(@RequestParam Optional<String> name) {
+        log.info("getAllUsers called");
+        log.debug("getAllUsers called with name filter: {}", name.orElse("nene"));
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable long id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@PathVariable long id, @RequestBody User updatedUser) {
+        return userService.updateUser(id, updatedUser)
+                .orElse(null);
+    }
+}
---
+++ mycode/myapp/src/main/java/com/sk/skala/myapp/service/UserService.java
@@ -0,0 +1,45 @@
+package com.sk.skala.myapp.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+import com.sk.skala.myapp.domain.User;
+import com.sk.skala.myapp.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+
+    public Optional<User> updateUser(long id, User updateUser) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            return Optional.empty();
+        }
+        User user = optionalUser.get();
+        user.setName(updateUser.getName());
+        user.setEmail(updateUser.getEmail());
+        User savedUser = userRepository.save(user);
+        return Optional.of(savedUser);
+    }
+}
